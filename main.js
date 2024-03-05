cc7f8610c723ee7fcf952c57f496112bb72d0875
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-03-04 16:38:08 -0800

    I want to learn git and github so I add this commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let numberfirt = Number (prompt('enter first number ='));
+let numbersecond =Number (prompt('enter second number')) ;
+
+let a, b, c,sum;
+sum = numberfirt + numbersecond;
+a = numberfirt - numbersecond;
+b = numberfirt / numbersecond;
+c = numberfirt * numbersecond;
+
+
+
+console.log(sum);
+console.log(a);
+console.log(b);
+console.log(b);
